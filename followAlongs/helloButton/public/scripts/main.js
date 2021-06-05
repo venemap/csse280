81f608ee98654dc725df56b8acd82f2f2ce8f33e
venemap <venemap@rose-hulman.edu>
2021-06-05 18:52:31 -0400

    buttons functional

--- followAlongs/helloButton/public/scripts/main.js
+++ followAlongs/helloButton/public/scripts/main.js
@@ -1,5 +1,28 @@
+var counter = 0;
+
 function main(){
     console.log("Ready");
+
+    document.querySelector("#decButton").onclick = (event) => {
+        counter -= 1;
+        updateView()
+      };
+    document.querySelector("#incButton").onclick = (event) => {
+        console.log("dec");  
+        counter += 1;
+        updateView()
+      };
+    document.querySelector("#resetButton").onclick = (event) => {
+        counter = 0;
+        updateView();
+      };
+
+}
+
+function updateView(){
+    // document.querySelector("#counter").innerHTML = "Count = " + counter;
+    document.querySelector("#counter").innerHTML = `Count = ${counter};`
+    
 }
 
 main()
