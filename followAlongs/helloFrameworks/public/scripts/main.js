5644fb1dd63b5526df05a272453651179eecf0e8
venemap <venemap@rose-hulman.edu>
2021-06-07 21:14:47 -0400

    converted to jQuery

--- followAlongs/helloFrameworks/public/scripts/main.js
+++ followAlongs/helloFrameworks/public/scripts/main.js
@@ -24,7 +24,8 @@ rhit.updateCounter = function(amt, isMult) {
 	} else{
 		rhit.counter += amt;
 	};
-	document.querySelector("#counter").innerHTML = `Count = ${rhit.counter}`;
+	//document.querySelector("#counter").innerHTML = `Count = ${rhit.counter}`;
+	$("#counter").html(`Count = ${rhit.counter}`);
 };
 
 /* Main */
@@ -34,15 +35,22 @@ rhit.main = function () {
 	console.log("Ready");
 	const buttons = document.querySelectorAll("#counterButtons button");
 
-	for (const button of buttons){
-		button.onclick = (event) => {
-			const dataAmount = parseInt(button.dataset.amount);
-			const dataIsMultiplication = button.dataset.isMultiplication;
-			// console.log(`Amount: ${dataAmount} isMult: ${dataIsMultiplication}`);
+	// for (const button of buttons){
+	// 	button.onclick = (event) => {
+	// 		const dataAmount = parseInt(button.dataset.amount);
+	// 		const dataIsMultiplication = button.dataset.isMultiplication;
+	// 		// console.log(`Amount: ${dataAmount} isMult: ${dataIsMultiplication}`);
 
-			rhit.updateCounter(dataAmount, dataIsMultiplication)
-		};
-	};
+	// 		rhit.updateCounter(dataAmount, dataIsMultiplication)
+	// 	};
+	// };
+	
+	$("#counterButtons button").click((event) => {
+		const dataAmount = $(event.target).data("amount");
+		const dataIsMultiplication = !!$(event.target).data("isMultiplication");
+		// console.log(`Amount: ${dataAmount} isMult: ${dataIsMultiplication}`);
+		rhit.updateCounter(dataAmount, dataIsMultiplication);
+	})
 };
 
 rhit.main();
